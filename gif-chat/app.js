dac370a26eacfc0bea8821f4f462fad6aaf6e4c0
Seoyoung Jo <josy3465@naver.com>
2020-10-15 16:58:21 +0900

    socket.io를 이용한 채팅방(개정 전 chapter11)

--- gif-chat/app.js
+++ gif-chat/app.js
@@ -10,6 +10,7 @@ require('dotenv').config();
 
 const webSocket = require('./socket');
 const indexRouter = require('./routes');
+const connect = require('./schemas');
 // const usersRouter = require('./routes/users');
 
 const app = express();
@@ -32,19 +33,22 @@ app.set('port', process.env.PORT || 8005);
 
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/gif', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
-  resave: false,
-  saveUninitialized: false,
-  secret: process.env.COOKIE_SECRET,
-  cookie: {
-    httpOnly: true,
-    secure: false,
-  },
-}));
+app.use(sessionMiddleware);
+// app.use(session({
+//   resave: false,
+//   saveUninitialized: false,
+//   secret: process.env.COOKIE_SECRET,
+//   cookie: {
+//     httpOnly: true,
+//     secure: false,
+//   },
+// }));
 app.use(flash());
+
 
 app.use((req, res, next) => {
   if(!req.session.color) {
